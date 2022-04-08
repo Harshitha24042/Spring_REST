5d32ad2cfc5608a7220917559250fec6dae29b81
harshitha <harshithar242000@gmail.com>
2022-04-09 05:19:03 +0530

    Security

---
+++ src/Q1/Helloworldcontroller.java
@@ -0,0 +1,11 @@
+package Q1;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Helloworldcontroller {
+	 @RequestMapping("/hello")
+	  public String dispaly() {
+		  return "Hello World!";
+	  }
+}
---
+++ src/Q5/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package Q5;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import REST.SpringRESTassignments.Q7.Order;
+
+public interface EmployeeRepo extends MongoRepository<Employee, String> {
+    
+}
